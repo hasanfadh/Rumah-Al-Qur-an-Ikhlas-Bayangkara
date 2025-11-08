46708b9e203163ed5b02faf01adea87377898317
Hasan Fadhlurrahman <hasan.fadlurrahman@gmail.com>
2025-11-08 12:51:27 +0700

    Update logo

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rumah Al-Qur'an Ikhlas Bayangkara</title>
-    <link rel="icon" type="image.png" href="{{ asset('logo.png') }}">
+    <link rel="icon" href="{{ asset('icon.ico') }} type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         html { scroll-behavior: smooth; }
