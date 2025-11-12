b7302a2729ec07d127ccd39821e223eae24208e4
Hasan Fadhlurrahman <hasan.fadlurrahman@gmail.com>
2025-11-12 18:51:19 +0700

    Try

--- api/index.js
+++ api/index.js
@@ -1,11 +1,4 @@
-import { execSync } from "child_process";
-import { readFileSync } from "fs";
-
 export default function handler(req, res) {
-  try {
-    const output = execSync("php api/index.php", { encoding: "utf8" });
-    res.status(200).send(output);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+  // Ini sama seperti echo di PHP
+  res.status(200).send("Halo, PHP dari Vercel!");
 }
